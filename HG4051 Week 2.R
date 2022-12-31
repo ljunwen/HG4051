ced6f7d5b954392dcd9c2f8a3a22b3dc514f7d58
ljunwen <120661365+ljunwen@users.noreply.github.com>
2022-12-31 15:28:35 +0800

    Create HG4051 Week 2.R

---
+++ HG4051 Week 2.R
@@ -0,0 +1,42 @@
+# downloads the data file from the course's GitHub
+download.file("https://github.com/ljunwen/HG4051/raw/main/data/Week%202%20-%20RT%20data.txt", "reaction time.txt")
+
+# simulation of distribution of sample means
+
+mean <- 0
+stdev <- 2
+
+# create a dataset of 1000 numbers from a normal distribution of mean 'mean' and standard deviation 'stdev'
+x <- rnorm(1000, mean, stdev)
+dist <- as.data.frame(x)
+
+# plot a histogram of the dataset
+hist(dist$x)
+
+# create a dataset of sample means with sample size 'n' and the number of samples 'num_samples'
+n <- 5
+num_samples <- 5000
+
+samples <- data.frame(matrix(vector(), num_samples, 1, dimnames=list(c(), "mean")))
+
+for (i in seq_along(1:(num_samples))) {
+   samples$mean[i] <- mean(sample(dist$x, n))  
+}
+
+hist(samples$mean)
+
+# calculates the actual standard deviation of the sample means as well as the calculated standard deviation from the population standard deviation
+sd(samples$mean)
+stdev/sqrt(n)
+
+
+# t-tests
+path <- ""
+Data <- read.delim(file = paste0(path, "reaction time.txt"), header = TRUE, stringsAsFactors = TRUE)
+
+t.test(RT ~ Drug, data = Data)
+t.test(RT ~ Drug, paired = TRUE, data = Data)
+
+# non-parametric tests
+wilcox.test(RT ~ Drug, data = Data)
+wilcox.test(RT ~ Drug, paired = TRUE, data = Data)
