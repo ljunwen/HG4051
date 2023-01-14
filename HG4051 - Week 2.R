089a836e61ca17d1a750679995bc7fd76f1a4912
ljunwen <120661365+ljunwen@users.noreply.github.com>
2023-01-14 09:41:50 +0800

    Update HG4051 - Week 2.R

--- HG4051 - Week 2.R
+++ HG4051 - Week 2.R
@@ -1,5 +1,5 @@
 # downloads the data file from the course's GitHub
-download.file("https://github.com/ljunwen/HG4051/raw/main/data/Week%202%20-%20RT%20data.txt", "Week 2 - RT data.txt")
+download.file("https://github.com/ljunwen/HG4051/raw/main/data/Week%202%20-%20RT%20data.txt", "Week 2 - RT data.txt", method = "libcurl")
 
 # simulation of distribution of sample means
 
