0ae3dfebb152b7588f9f14f8af7ae341da8d9087
ljunwen <120661365+ljunwen@users.noreply.github.com>
2023-02-24 01:29:00 +0800

    Add files via upload

---
+++ HG4051 Week 7 (stats).R
@@ -0,0 +1,87 @@
+# downloads the data file from the course's GitHub
+download.file("https://github.com/ljunwen/HG4051/raw/main/data/Week%207%20-%20Ratings%20data.txt", "Week 6 - RT data.txt", method = "libcurl")
+download.file("https://github.com/ljunwen/HG4051/raw/main/data/Week%207%20-%20Lah%20examples.txt", "Week 7 - lah examples.txt", method = "libcurl")
+
+# transformations
+
+path <- ""
+examples <- read.delim(file = paste0(path, "Week 7 - Lah examples.txt"), header = TRUE, stringsAsFactors = TRUE)
+
+hist(as.data.frame(xtabs(~ SCD, data = examples))$Freq)
+shapiro.test(as.data.frame(xtabs(~ SCD, data = examples))$Freq)
+hist(log10(as.data.frame(xtabs(~ SCD, data = examples))$Freq))
+shapiro.test(log10(as.data.frame(xtabs(~ SCD, data = examples))$Freq))
+
+
+# mixed-effects models
+
+Data <- read.delim(file = paste0(path, "Week 7 - Ratings data.txt"), header = TRUE, stringsAsFactors = TRUE)
+Data$Speaker <- as.factor(Data$Speaker)
+Data$Sentence <- as.factor(Data$Sentence)
+Data$Loop <- as.factor(Data$Loop)
+
+if(!require(lmerTest)){
+  install.packages("lmerTest")   # installs the 'lmerTest' package if it isn't installed
+  library(lmerTest)   # loads the package
+}
+
+if(!require(car)){
+  install.packages("car")   # installs the 'car' package if it isn't installed
+  library(car)   # loads the package
+}
+
+if(!require(MuMIn)){
+  install.packages("MuMIn")   # installs the 'MuMIn' package if it isn't installed
+  library(MuMIn)   # loads the package
+}
+
+
+# random intercepts
+
+full.lm <- lm(RC3 ~ S.Gender + S.Ethnicity + S.Education + S.AgeGroup + P.Gender + P.Ethnicity + P.Education + P.Age, data = Data)
+car::Anova(full.lm, type = "II")
+
+ResponseId.lmer <- lmer(RC3 ~ S.Gender + S.Ethnicity + S.Education + S.AgeGroup + P.Gender + P.Ethnicity + P.Education + P.Age + (1|ResponseId), data = Data, REML = TRUE, control = lmerControl(optimizer = 'bobyqa', optCtrl=list(maxfun=2e5)))
+car::Anova(ResponseId.lmer, type = "II")
+
+Speaker.lmer <- lmer(RC3 ~ S.Gender + S.Ethnicity + S.Education + S.AgeGroup + P.Gender + P.Ethnicity + P.Education + P.Age + (1|Speaker), data = Data, REML = TRUE, control = lmerControl(optimizer = 'bobyqa', optCtrl=list(maxfun=2e5)))
+car::Anova(Speaker.lmer, type = "II")
+
+full.lmer <- lmer(RC3 ~ S.Gender + S.Ethnicity + S.Education + S.AgeGroup + P.Gender + P.Ethnicity + P.Education + P.Age + (1|ResponseId) + (1|Speaker), data = Data, REML = TRUE, control = lmerControl(optimizer = 'bobyqa', optCtrl=list(maxfun=2e5)))
+car::Anova(full.lmer, type = "II")
+
+anova(ResponseId.lmer, full.lmer, refit = FALSE)
+anova(Speaker.lmer, full.lmer, refit = FALSE)
+
+
+# random slopes
+
+full.lmer <- lmer(RC3 ~ S.Gender + S.Ethnicity + S.Education + S.AgeGroup + P.Gender + P.Ethnicity + P.Education + P.Age + (1 + S.Education|ResponseId) + (1|Speaker), data = Data, REML = TRUE, control = lmerControl(optimizer = 'bobyqa', optCtrl=list(maxfun=2e5)))
+reduced.lmer <- lmer(RC3 ~ S.Gender + S.Ethnicity + S.Education + S.AgeGroup + P.Gender + P.Ethnicity + P.Education + P.Age + (1|ResponseId) + (1|Speaker), data = Data, REML = TRUE, control = lmerControl(optimizer = 'bobyqa', optCtrl=list(maxfun=2e5)))
+anova(reduced.lmer, full.lmer, refit = FALSE)
+
+car::Anova(full.lmer, type = "II")
+
+
+
+# random intercepts - 2nd run
+
+ResponseId.lmer <- lmer(RC3 ~ S.Gender + P.Age + (1|ResponseId), data = Data, REML = TRUE, control = lmerControl(optimizer = 'bobyqa', optCtrl=list(maxfun=2e5)))
+Speaker.lmer <- lmer(RC3 ~ S.Gender + P.Age + (1|Speaker), data = Data, REML = TRUE, control = lmerControl(optimizer = 'bobyqa', optCtrl=list(maxfun=2e5)))
+full.lmer <- lmer(RC3 ~ S.Gender + P.Age + (1|ResponseId) + (1|Speaker), data = Data, REML = TRUE, control = lmerControl(optimizer = 'bobyqa', optCtrl=list(maxfun=2e5)))
+anova(ResponseId.lmer, full.lmer, refit = FALSE)
+anova(Speaker.lmer, full.lmer, refit = FALSE)
+
+
+# random slopes - 2nd run
+
+full.lmer <- lmer(RC3 ~ S.Gender + P.Age + (1 + S.Education|ResponseId) + (1|Speaker), data = Data, REML = TRUE, control = lmerControl(optimizer = 'bobyqa', optCtrl=list(maxfun=2e5)))
+reduced.lmer <- lmer(RC3 ~ S.Gender + P.Age + (1|ResponseId) + (1|Speaker), data = Data, REML = TRUE, control = lmerControl(optimizer = 'bobyqa', optCtrl=list(maxfun=2e5)))
+anova(reduced.lmer, full.lmer, refit = FALSE)
+
+car::Anova(full.lmer, type = "II")
+
+
+# marginal and conditional R^2
+
+r.squaredGLMM(full.lmer)
---
+++ HG4051 Week 7 - likelihood ratio ranking script.R
@@ -0,0 +1,64 @@
+# import the AntConc wordlist and collocates list to R
+
+path <- "D:/"
+wordlist <- read.delim(file = paste0(path, "antconc_wordlist.txt"), header = FALSE, stringsAsFactors = FALSE)
+collocates <- read.delim(file = paste0(path, "antconc_results_tall.txt"), header = FALSE, stringsAsFactors = FALSE)
+word <- "tall"
+write_to_file <- "n"
+
+
+# adjusts the columns of wordlist and removes non-Unicode words
+
+wordlist <- wordlist[-c(1:3), -c(1,4)]
+wordlist <- wordlist[, c(2,1)]
+colnames(wordlist) <- c("Word", "Frequency")
+wordlist <- subset(wordlist, grepl("[a-z]+",wordlist$Word))
+
+
+# adjusts the columns of the AntConc collocates
+
+collocates <- collocates[-c(1:2), -1]
+colnames(collocates) <- c("Frequency", "Freq_L", "Freq_R", "MI_stat", "Collocate")
+collocates$LR_test <- NA
+collocates$Fisher_test <- NA
+
+
+# loads required packages
+
+if(!require(lmtest)){
+  install.packages("lmtest")   # installs the 'lmtest' package if it isn't installed
+  library(lmtest)   # loads the package
+}
+
+if(!require(DescTools)){
+  install.packages("DescTools")   # installs the 'DescTools' package if it isn't installed
+  library(DescTools)   # loads the package
+}
+
+
+# calculates likelihood ratio statistic for each collocate in the list
+
+xtabs_table <- data.frame(matrix(vector(), 4, 3, dimnames=list(c(), c("Word.A","Word.B","Total"))),stringsAsFactors=T)
+
+for (i in seq_along(collocates$Collocate)) {
+   xtabs_table[1,] <- c("Yes", "Yes", collocates$Frequency[i])
+   xtabs_table[2,] <- c("Yes", "No", sum(collocates$Frequency) - collocates$Frequency[i])
+   xtabs_table[3,] <- c("No", "Yes", subset(wordlist, wordlist$Word == collocates$Collocate[i])$Frequency)
+   xtabs_table[4,] <- c("No", "No", sum(wordlist$Frequency) - subset(wordlist, wordlist$Word == collocates$Collocate[i])$Frequency - subset(wordlist, wordlist$Word == word)$Frequency)
+   xtabs_table$Total <- as.numeric(xtabs_table$Total)
+
+   collocates$LR_test[i] <- GTest(xtabs(Total ~ Word.A + Word.B, data = xtabs_table))$statistic
+   collocates$Fisher_test[i] <- fisher.test(xtabs(Total ~ Word.A + Word.B, data = xtabs_table))$p.value
+}
+
+collocates <- with(collocates, collocates[order(LR_test, decreasing = TRUE),])
+# collocates <- with(collocates, collocates[order(Fisher_test, decreasing = FALSE),])
+
+rm(xtabs_table)
+
+
+# outputs data to text file if write_to_file = "y"
+
+if (write_to_file == "y") {
+   write.table(collocates, paste0(path, "likelihood ratio ranking.txt"), sep = "\t", row.names = FALSE)
+}
